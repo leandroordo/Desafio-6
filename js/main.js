d3816357d7a1738d44f0a667809881aa2ae21d72
Leandro Ordoñez <leandro.o.ar@hotmail.com>
2023-11-26 15:35:09 -0300

    Ejercicio 1

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function main() {
   const pageTitleDiv = document.getElementById("page-title");
   displayTitle(pageTitleDiv);
 
-  //Leer los datos de los integrantes y mostrarlos
+  //Leer con JS, ni bien se cargue la página, los datos definidos en el HTML
   const primerIntegranteDiv = document.getElementById("primer-integrante");
   const segundoIntegranteDiv = document.getElementById("segundo-integrante");
   const integrantes = procesarIntegrantes(
@@ -11,21 +11,16 @@ function main() {
     segundoIntegranteDiv
   );
 
-  //Ver si coinciden los nombres
-  const coincidencias = datosCoinciden(
-    integrantes[0].primerNombre,
-    integrantes[0].segundoNombre,
-    integrantes[1].primerNombre,
-    integrantes[1].segundoNombre
-  );
-  if (coincidencias.coincidencia) {
-    //Encontramos una coincidencia
-    document.getElementById("aviso-coincidencia").className = "aviso visible";
-    console.log(
-      `Hay concidencias en los nombres de los integrantes: ${coincidencias.valor}`
-    );
-  } else {
-    document.getElementById("aviso-coincidencia").className = "aviso hidden";
+  //Analizar coincidencias en los nombres
+  //Ver si los nombres cargados en el HTML coinciden en algún caso
+  coincidenciaNombres(integrantes);
+
+  //Consultar a través de un confirm si se desea comparar los apellidos
+  //En caso contrario no tomar ninguna acción
+  if (window.confirm("¿Desea comparar también los apellidos?")) {
+    //Rrealizar la comparación de apellidos, con el mismo criterio que con los nombres
+    //y realizar las mismas acciones
+    coincidenciaApellidos(integrantes);
   }
 }
 
@@ -38,14 +33,20 @@ function displayTitle(outputDiv) {
 
 function procesarIntegrantes(outputDiv1, outputDiv2) {
   //Obtener los datos de las listas
-  var integrantes = getIntegrantes("description-lists");
+  var listContainer = document.getElementById("description-lists");
+  var integrantes = getIntegrantes(listContainer);
 
+  /*La generación del string con el nombre completo que se mostrará en consola del integrante
+  no debe repetirse, sino lograr que una función lo lleve a cabo y simplemente
+  llamarla una vez para cada integrante*/
   const primerIntegrante = getDatosIntegrante(integrantes[0]);
   const segundoIntegrante = getDatosIntegrante(integrantes[1]);
 
   outputDiv1.innerHTML = primerIntegrante;
   outputDiv2.innerHTML = segundoIntegrante;
 
+  //Informar en la consola los nombres completos de cada integrante.
+  //Debe utilizarse un único llamado a console.log para generar los 4 (2!) renglones
   const result = `Integrante 1: ${primerIntegrante}\nIntegrante 2: ${segundoIntegrante}`;
   console.log(result);
 
@@ -56,7 +57,7 @@ function getIntegrantes(containerDiv) {
   const integrantes = [];
   let primerNombre, segundoNombre, primerApellido, segundoApellido;
 
-  var items = document.getElementById(containerDiv).children;
+  var items = containerDiv.children;
   for (let i = 0; i < items.length; i++) {
     const listItem = items[i];
 
@@ -102,7 +103,6 @@ function getIntegrantes(containerDiv) {
         childIndex++;
       }
 
-      debugger;
       //Agregar los datos del integrante al array de integrantes
       const integrante = {
         primerNombre,
@@ -138,6 +138,60 @@ function getDatosIntegrante(integrante) {
   return nombreCompleto.trim();
 }
 
+function coincidenciaNombres(integrantes) {
+  //Ver si coinciden los nombres
+  const coincidencias = datosCoinciden(
+    integrantes[0].primerNombre,
+    integrantes[0].segundoNombre,
+    integrantes[1].primerNombre,
+    integrantes[1].segundoNombre
+  );
+  if (coincidencias.coincidencia) {
+    //Encontramos una coincidencia
+    //Avisar de la coincidencia y pedir un color
+    color = notificarCoincidencia("N", coincidencias.valor);
+    if (color) {
+      var listContainer = document.getElementById("description-lists");
+      destacarLista(listContainer, coincidencias.valor, color);
+    }
+  } else {
+    document.getElementById("aviso-coincidencia-nombres").className =
+      "aviso hidden";
+
+    //Si, en cambio, no hay coincidencias, informar en la consola que no las hubo.
+    console.log(
+      "Lo siento, pero no hubo concidencias en los nombres de los integrantes."
+    );
+  }
+}
+
+function coincidenciaApellidos(integrantes) {
+  //Ver si coinciden los apellidos
+  const coincidencias = datosCoinciden(
+    integrantes[0].primerApellido,
+    integrantes[0].segundoApellido,
+    integrantes[1].primerApellido,
+    integrantes[1].segundoApellido
+  );
+  if (coincidencias.coincidencia) {
+    //Encontramos una coincidencia
+    //Avisar de la coincidencia y pedir un color
+    color = notificarCoincidencia("A", coincidencias.valor);
+    if (color) {
+      var listContainer = document.getElementById("description-lists");
+      destacarLista(listContainer, coincidencias.valor, color);
+    }
+  } else {
+    document.getElementById("aviso-coincidencia-apellidos").className =
+      "aviso hidden";
+
+    //Si, en cambio, no hay coincidencias, informar en la consola que no las hubo.
+    console.log(
+      "Lo siento, pero no hubo concidencias en los apellidos de los integrantes."
+    );
+  }
+}
+
 function datosCoinciden(n1, n2, n3, n4) {
   if (n1 === n3 || n1 == n4)
     return {
@@ -150,6 +204,52 @@ function datosCoinciden(n1, n2, n3, n4) {
       coincidencia: true,
       valor: n2,
     };
+}
+
+function notificarCoincidencia(tipo, valorRepetido) {
+  const nombresOApellidos = tipo === "N" ? "nombres" : "apellidos";
+  document.getElementById(`aviso-coincidencia-${nombresOApellidos}`).className =
+    "aviso visible";
+
+  //Informar en la consola que hubo coincidencias
+  console.log(
+    `Hay concidencias en los ${nombresOApellidos} de los integrantes: ${valorRepetido}`
+  );
+
+  //Informar en un prompt que hubo coincidencias y solicitar en ese mismo prompt que se ingrese un color
+  //para destacar los nombres (o apellidos)
+  return window.prompt(
+    `Hay concidencias en los ${nombresOApellidos} de los integrantes: ${valorRepetido}. 
+    Escriba el código HTML válido de un color para destacar las coincidencias.`
+  );
+}
+
+function destacarLista(containerDiv, valor, color) {
+  var items = containerDiv.children;
+  for (let i = 0; i < items.length; i++) {
+    const listItem = items[i];
+
+    if (listItem.tagName == "DL") {
+      let childIndex = 0;
+
+      while (childIndex < items[i].children.length) {
+        let element = listItem.children[childIndex];
+        if (element.tagName == "DT") {
+          childIndex++;
+          element = listItem.children[childIndex];
+
+          if (element.tagName == "DD") {
+            if (element.innerText === valor) {
+              //Agregarle estilos en línea a los nombres coincidentes, de tal manera que se destaquen en color
+              //los items de listas cuyos nombres (o apellidos) coincidan
+              element.style.color = color;
+            }
+          }
+        }
+        childIndex++;
+      }
+    }
+  }
 }
 
 //Invocar a la función main
