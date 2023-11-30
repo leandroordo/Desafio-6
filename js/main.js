4c8a0541af9fc71c109e1f5d50c84e6ff9d386cd
Leandro Ordoñez <leandro.o.ar@hotmail.com>
2023-11-30 19:48:22 -0300

    Corrijo bug para casos de nombres vacíos

--- js/main.js
+++ js/main.js
@@ -219,13 +219,13 @@ function coincidenciaNombresApellidos(integrantes, configDestino) {
 }
 
 function datosCoinciden(n1, n2, n3, n4) {
-  if (n1 === n3 || n1 == n4)
+  if (n1 && (n1 === n3 || n1 == n4))
     return {
       coincidencia: true,
       valor: n1,
     };
 
-  if (n2 === n3 || n2 == n4)
+  if (n2 && (n2 === n3 || n2 == n4))
     return {
       coincidencia: true,
       valor: n2,
